cd75743528b3be3b412f0b6418ccec8ae2c86d56
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-06-25 12:36:37 +0800

    move styles to bottom

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,24 @@
-import Constants from 'expo-constants';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Constants.statusBarHeight,
-    flexGrow: 1,
-    flexShrink: 1,
-  },
-});
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <Text>Rate Repository Application</Text>
+      <AppBar />
       <View>
         <RepositoryList />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
 
 export default Main;
