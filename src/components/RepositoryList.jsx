bfbeeba8fc8cf26a8ad99c76b62b6b4acbd867b7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-09 13:34:12 +0800

    fetch data from URL instead of locally defined data

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,33 @@
 import { FlatList, StyleSheet, View } from 'react-native';
 import RepositoryItem from './RepositoryItem';
-import { repositories } from '../../repositories';
+// import { repositories } from '../../repositories';
+import { useEffect, useState } from 'react';
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 export default function RepositoryList() {
+  const [repositories, setRepositories] = useState();
+
+  const fetchRepositories = async () => {
+    const uri = 'http://192.168.0.161:5000/api/repositories';
+    const response = await fetch(uri);
+    const json = await response.json();
+    console.log(json)
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  // Get nodes from the edges array
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem repository={item} />}
       keyExtractor={(repository) => repository.id}
