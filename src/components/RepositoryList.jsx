2560d04142fe1f4c10fdf984464c49e89e280eb3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-11 21:44:16 +0800

    fetching repo function is now a separate hook

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,23 +2,12 @@ import { FlatList, StyleSheet, View } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 // import { repositories } from '../../repositories';
 import { useEffect, useState } from 'react';
+import useRepositories from '../hooks/useRepositories';
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    const uri = 'http://192.168.0.161:5000/api/repositories';
-    const response = await fetch(uri);
-    const json = await response.json();
-    console.log(json)
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
+  const { repositories } = useRepositories();
 
   // Get nodes from the edges array
   const repositoryNodes = repositories
