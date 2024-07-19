9357263afd330eae94cfda91cec9f06bd91a811c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-19 15:07:34 +0700

    useAuthStorage instead of directly using AuthStorageContext for authStorage

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,12 @@
 import { useMutation } from '@apollo/client';
 import { SIGN_IN } from '../graphql/mutations';
+// import { useContext } from 'react';
+// import AuthStorageContext from '../contexts/AuthStorageContext';
+import useAuthStorage from './useAuthStorage';
 
 const useSignIn = () => {
+  // const authStorage = useContext(AuthStorageContext);
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(SIGN_IN, {
     onError: (error) => {
       console.error(error);
