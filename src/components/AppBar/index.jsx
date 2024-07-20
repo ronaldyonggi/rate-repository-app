1a8b3e721c919551c15cffa765d3654539b26579
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-20 09:02:06 +0700

    10.16 render Sign out tab if not logged in

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,14 +1,21 @@
-import { StyleSheet, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Pressable, ScrollView, Text } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../../theme';
 import Tab from './Tab';
+import { useQuery } from '@apollo/client';
+import { ME } from '../../graphql/queries';
+import SignOutTab from './SignOutTab';
 
 export default function AppBar() {
+  const { loading, data } = useQuery(ME);
+
+  if (loading) return <Text>Loading...</Text>;
+  
   return (
     <Pressable style={styles.container}>
       <ScrollView horizontal style={styles.scrollView}>
         <Tab title='Repositories' path={'/'} />
-        <Tab title='Sign in' path={'/sign-in'} />
+        {data.me? <SignOutTab /> : <Tab title='Sign in' path={'/sign-in'} />}
       </ScrollView>
     </Pressable>
   );
