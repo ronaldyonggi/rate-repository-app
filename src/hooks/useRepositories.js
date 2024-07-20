341ec19d97d25160e6ecf1807edab2696c2bac79
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-20 21:24:43 +0700

    add GQL implementation

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,5 +1,9 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+/* Implementation using REST API
 export default function useRepositories() {
   const [repositories, setRepositories] = useState();
   const [loading, setLoading] = useState(false);
@@ -19,4 +23,14 @@ export default function useRepositories() {
   }, []);
 
   return { repositories, loading, refetch: fetchRepositories };
+}
+*/
+
+// Implementation using GraphQL
+export default function useRepositories() {
+  const { loading, data } = useQuery(GET_REPOSITORIES);
+
+  if (loading) return { repositories: [], loading };
+
+  return { repositories: data.repositories, loading };
 }
