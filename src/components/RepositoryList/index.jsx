64cdb6aec1761f9ee2b9d246d8d3913d3d37581b
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-21 18:28:48 +0700

    10.17 refactor RepositoryList to use container

---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,14 @@
+import { Text } from 'react-native';
+import useRepositories from '../../hooks/useRepositories';
+import RepositoryListContainer from './RepositoryListContainer';
+
+
+export default function RepositoryList() {
+  const { repositories, loading } = useRepositories();
+
+  if (loading) return <Text>Loading...</Text>;
+
+  return <RepositoryListContainer repositories={repositories} />
+
+}
+
