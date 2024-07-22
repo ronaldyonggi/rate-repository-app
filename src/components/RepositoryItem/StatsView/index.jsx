843132826d242f7a29d909ea92db73849f021d04
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-22 12:39:43 +0700

    bugfix round up when count is >= 1000, otherwise return count

--- src/components/RepositoryItem/StatsView/index.jsx
+++ src/components/RepositoryItem/StatsView/index.jsx
@@ -9,9 +9,11 @@ export default function StatsView({
   ratingAverage,
 }) {
   const countConverter = (count) => {
-    if (count > 1000) {
+    if (count >= 1000) {
       return `${(count / 1000).toFixed(1)}k`;
     }
+    return count
+
   };
   return (
     <View style={styles.container}>
