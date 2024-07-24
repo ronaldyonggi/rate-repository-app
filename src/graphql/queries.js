9638c4fa400a7dc98941318340fb55f907d82de7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-24 15:33:21 +0700

    10.19 defined GET_SINGLE_REPOSITORY query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -7,12 +7,20 @@ export const GET_REPOSITORIES = gql`
       edges {
         node {
           ...RepositoryDetails
+        }
       }
     }
   }
-  }
   ${REPOSITORY_DETAILS}
 `;
+
+export const GET_SINGLE_REPOSITORY = gql`
+  query getSingleRepository($id: ID!) {
+    repository(id: $id) {
+      ...RepositoryDetails
+    }
+  }
+  ${REPOSITORY_DETAILS}
 `;
 
 export const ME = gql`
