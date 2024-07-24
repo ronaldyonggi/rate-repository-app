15dcca5c224c0d1ed2ff606697d83905d75b3af7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-24 15:34:02 +0700

    reorganize tests to within __tests__ dir

--- src/__tests__/components/RepositoryList.test.jsx
+++ src/__tests__/components/RepositoryList.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, within } from '@testing-library/react-native';
-import RepositoryListContainer from './RepositoryListContainer';
+import RepositoryListContainer from '../../components/RepositoryList/RepositoryListContainer';
 
 describe('RepositoryList', () => {
   describe('RepositoryListContainer', () => {
