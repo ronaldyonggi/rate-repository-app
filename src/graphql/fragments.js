2621db3e134cb25e4e1831f5de001575bfa70da9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-07-26 09:59:52 +0700

    define REVIEW_DETAILS fragment

--- src/graphql/fragments.js
+++ src/graphql/fragments.js
@@ -1,17 +1,38 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
+export const REVIEW_DETAILS = gql`
+  fragment ReviewDetails on Review {
+    id
+    text
+    rating
+    createdAt
+    user {
+      id
+      username
+    }
+  }
+`;
 
 export const REPOSITORY_DETAILS = gql`
-    fragment RepositoryDetails on Repository {
-        id
-        fullName
-        description
-        language
-        stargazersCount
-        forksCount
-        reviewCount
-        ratingAverage
-        ownerAvatarUrl
-        url
+  fragment RepositoryDetails on Repository {
+    id
+    fullName
+    description
+    language
+    stargazersCount
+    forksCount
+    reviewCount
+    ratingAverage
+    ownerAvatarUrl
+    url
+    reviews {
+      edges {
+        node {
+          ...ReviewDetails
+        }
+      }
     }
-`
+  }
+  ${REVIEW_DETAILS}
+`;
+
