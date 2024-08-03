28dbdb36224e8d06629383fa6395f7dde88977e4
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-03 14:52:39 +0800

    10.22 defined SIGN_UP mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -9,9 +9,18 @@ export const SIGN_IN = gql`
 `;
 
 export const CREATE_REVIEW = gql`
-  mutation createReview($review: CreateReviewInput) {
+  mutation createReview($review: CreateReviewInput!) {
     createReview(review: $review) {
       repositoryId
+    }
+  }
+`;
+
+export const SIGN_UP = gql`
+  mutation createUser($user: CreateUserInput!) {
+    createUser(user: $user) {
+      reviewCount
+      username
     }
   }
 `;
