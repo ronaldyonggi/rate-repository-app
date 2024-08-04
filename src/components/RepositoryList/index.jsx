e009ead6d2787f0283ff195fc2c8b7cfb4ef7d52
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-04 13:25:01 +0800

    10.23 implement principle state that's used for useRepositories hook

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,11 +1,19 @@
 import { Text } from 'react-native';
 import useRepositories from '../../hooks/useRepositories';
 import RepositoryListContainer from './RepositoryListContainer';
+import { useState } from 'react';
 
 export default function RepositoryList() {
-  const { repositories, loading } = useRepositories();
+  const [principle, setPrinciple] = useState('default');
+  const { repositories, loading } = useRepositories(principle);
 
   if (loading) return <Text>Loading...</Text>;
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      principle={principle}
+      setPrinciple={setPrinciple}
+    />
+  );
 }
