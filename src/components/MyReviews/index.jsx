1dfe1ee8dad233c3d34e0d7850f6a2a6da280880
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-06 19:19:29 +0800

    10.26 pass refetch function from gql query as props to ReviewItem comp

--- src/components/MyReviews/index.jsx
+++ src/components/MyReviews/index.jsx
@@ -4,10 +4,10 @@ import { useQuery } from '@apollo/client';
 import { GET_CURRENT_USER } from '../../graphql/queries';
 
 export default function MyReviews() {
-  const { loading, data } = useQuery(GET_CURRENT_USER, {
+  const { loading, data, refetch } = useQuery(GET_CURRENT_USER, {
     variables: {
       includeReviews: true,
-    },
+    }
   });
 
   if (loading) return <Text>Loading...</Text>;
@@ -19,7 +19,7 @@ export default function MyReviews() {
   return (
     <FlatList
       data={reviewNodes}
-      renderItem={({ item }) => <ReviewItem review={item} />}
+      renderItem={({ item }) => <ReviewItem review={item} refetch={refetch} />}
       keyExtractor={(review) => review.id}
     />
   );
