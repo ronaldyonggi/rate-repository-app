df6f189c8ae8291fd366401cf90b04899fbed526
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-06 19:20:00 +0800

    10.26 implement functional buttons for view review's repo and delete review

--- src/components/MyReviews/ReviewItem.jsx
+++ src/components/MyReviews/ReviewItem.jsx
@@ -1,30 +1,74 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable, Alert } from 'react-native';
 import React from 'react';
 import theme from '../../theme';
 import Text from '../Text';
 import { format } from 'date-fns';
+import { useNavigate } from 'react-router-native';
+import useDeleteReview from '../../hooks/useDeleteReview';
 
 const formatDate = (date) => {
   const dateObj = new Date(date);
   return format(dateObj, 'MMMM do, yyyy');
 };
 
-export default function ReviewItem({ review }) {
-  const { repository, createdAt, text, rating } = review;
+export default function ReviewItem({ review, refetch }) {
+  const navigate = useNavigate();
+  const [deleteReview] = useDeleteReview();
+  const { id, repository, createdAt, text, rating } = review;
   const formattedCreateAt = formatDate(createdAt);
+
+  const handleViewRepository = () => {
+    navigate(`/repository/${repository.id}`);
+  };
+
+  const handleDeleteReview = () => {
+    Alert.alert(
+      'Delete review',
+      'Are you sure you want to delete this review?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: async () => {
+            await deleteReview(id);
+            await refetch();
+          },
+          style: 'destructive', // iOS only
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.ratingContainer}>
-        <Text style={styles.ratingText} fontWeight='bold'>
-          {rating}
-        </Text>
+      <View style={styles.infoContainer}>
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingText} fontWeight='bold'>
+            {rating}
+          </Text>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text fontWeight='bold'>{repository.fullName}</Text>
+          <Text style={styles.createdAt} color='secondary'>
+            {formattedCreateAt}
+          </Text>
+          <Text>{text}</Text>
+        </View>
       </View>
-      <View style={styles.descriptionContainer}>
-        <Text fontWeight='bold'>{repository.fullName}</Text>
-        <Text style={styles.createdAt} color='secondary'>
-          {formattedCreateAt}
-        </Text>
-        <Text>{text}</Text>
+      <View style={styles.buttonsContainer}>
+        <Pressable onPress={handleViewRepository}>
+          <Text style={styles.buttonText} color='primary' fontWeight='bold'>
+            View repository
+          </Text>
+        </Pressable>
+        <Pressable onPress={handleDeleteReview}>
+          <Text style={styles.buttonText} color='danger' fontWeight='bold'>
+            Delete review
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -33,10 +77,11 @@ export default function ReviewItem({ review }) {
 const styles = StyleSheet.create({
   container: {
     marginTop: 10,
-    flex: 1,
     backgroundColor: theme.colors.repositoryItemBackground,
-    flexDirection: 'row',
     paddingVertical: 15,
+  },
+  infoContainer: {
+    flexDirection: 'row',
   },
   ratingContainer: {
     borderWidth: 2,
@@ -57,5 +102,13 @@ const styles = StyleSheet.create({
   createdAt: {
     marginTop: 1,
     marginBottom: 3,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 15,
+  },
+  buttonText: {
+    padding: 15,
   },
 });
