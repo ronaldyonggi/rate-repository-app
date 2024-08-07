5682d4ee1924e0084d359c36b3148a4473e795ba
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-07 14:52:20 +0800

    add relayStylePagination field caching policy

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,11 +1,22 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import Constants from 'expo-constants';
 import { setContext } from '@apollo/client/link/context';
+import { relayStylePagination } from '@apollo/client/utilities';
 
 const { apollo_uri } = Constants.expoConfig.extra;
 
 const httpLink = createHttpLink({
   uri: apollo_uri,
+});
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        repositories: relayStylePagination(),
+      },
+    },
+  },
 });
 
 /* PREVIOUS APOLLO SETUP
@@ -36,7 +47,7 @@ const createApolloClient = (authStorage) => {
 
   return new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    cache
   });
 };
 
