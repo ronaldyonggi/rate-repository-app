6c918d851aef9797c15a634a4801476a57edd248
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-07 14:53:42 +0800

    GET_REPOSITORIES query takes first and after, returns cursor and pageInfo

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -6,16 +6,26 @@ export const GET_REPOSITORIES = gql`
     $orderBy: AllRepositoriesOrderBy
     $orderDirection: OrderDirection
     $searchKeyword: String
+    $first: Int
+    $after: String
   ) {
     repositories(
       orderBy: $orderBy
       orderDirection: $orderDirection
       searchKeyword: $searchKeyword
+      first: $first
+      after: $after
     ) {
       edges {
         node {
           ...RepositoryDetails
         }
+        cursor
+      }
+      pageInfo {
+        endCursor
+        startCursor
+        hasNextPage
       }
     }
   }
