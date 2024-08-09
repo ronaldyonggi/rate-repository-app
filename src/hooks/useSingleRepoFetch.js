eb516278b78c332243195b6dc7b1860bf9e56780
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-09 18:35:58 +0800

    10.27 hook now has fetchMore functionality and accept first param

--- src/hooks/useSingleRepoFetch.js
+++ src/hooks/useSingleRepoFetch.js
@@ -1,13 +1,38 @@
 import { useQuery } from '@apollo/client';
 import { GET_SINGLE_REPOSITORY } from '../graphql/queries';
 
-export default function useSingleRepoFetch(repoId) {
-  const { loading, data } = useQuery(GET_SINGLE_REPOSITORY, {
-    variables: {
-      id: repoId,
-    },
+export default function useSingleRepoFetch({ repoId, first }) {
+  const queryVariables = {
+    id: repoId,
+    first,
+  };
+
+  const { loading, data, fetchMore } = useQuery(GET_SINGLE_REPOSITORY, {
+    variables: queryVariables,
     fetchPolicy: 'cache-and-network',
   });
 
-  return { repository: data ? data.repository : null, loading };
+  const handleFetchMore = () => {
+    const canFetchMore =
+      !loading && data && data.repository.reviews.pageInfo.hasNextPage;
+
+    if (!canFetchMore) {
+      return;
+    }
+
+    fetchMore({
+      variables: {
+        after: data.repository.reviews.pageInfo.endCursor,
+        ...queryVariables,
+      },
+    });
+  };
+
+  if (loading) return { repository: null, loading };
+
+  return {
+    repository: data ? data.repository : null,
+    loading,
+    fetchMore: handleFetchMore,
+  };
 }
